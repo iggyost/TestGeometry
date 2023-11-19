b2b21f1c87f2c880144a574427bd77d62b32dd3e
igoro <igoro@IGORPC>
2023-11-19 18:30:57 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using GeometryCalculateLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = ShapeClass.CreateCircle(5);
+            double areaCircle = ShapeClass.AreaCalculate(circle);
+            Console.WriteLine($"Площадь круга: {areaCircle}");
+            Triangle triangle = ShapeClass.CreateTriangle(3, 4, 5);
+            string isRight = triangle.IsRight() ? "Прямоугольный" : "Непрямоугольный";
+            double areaTriangle = ShapeClass.AreaCalculate(triangle);
+            Console.WriteLine($"Площадь треугольника: {areaTriangle} {isRight}");
+
+            Console.ReadKey();
+        }
+    }
+}
